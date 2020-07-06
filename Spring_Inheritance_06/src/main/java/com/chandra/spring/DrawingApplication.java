b1ae076669faf9ec781d0f520e181867000ec3d9
Chandu <67627909+chanduonline@users.noreply.github.com>
2020-07-06 18:40:31 +0530

    Add files via upload
    Spring Bean Inheritance Application

---
+++ Spring_Inheritance_06/src/main/java/com/chandra/spring/DrawingApplication.java
@@ -0,0 +1,15 @@
+package com.chandra.spring;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class DrawingApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+		Triangle t = context.getBean("triangle1",Triangle.class);
+		t.draw();
+		
+	}
+
+}
